dd2d608d1d2d9791994aae9f37ac1660a2834467
BinayakB <bishnu.binayak12@gmail.com>
2022-01-10 20:11:05 +0530

    demo code

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace LearnTwilio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            var accountSid = Secrets.accountSid;    //username
+            var authToken = Secrets.authToken;    //account password
+            var toNumber = Secrets.toNumber;
+            var fromNumber = Secrets.fromNumber;
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                to: new PhoneNumber(toNumber),
+                from: new PhoneNumber(fromNumber),
+                body: "Hi just learned Twilio messaging!"
+            );
+        }
+    }
+}
---
+++ Secrets.cs
@@ -0,0 +1,10 @@
+namespace LearnTwilio
+{
+    class Secrets
+    {
+        public static string accountSid { get; set; } = "ACbfff6dd8f652950fdcbafd65030554e4";
+        public static string authToken { get; set; } = "883854fb296ea47e363fb3ec0b9a9450";
+        public static string toNumber { get; set; } = "(+91 9836042209)";
+        public static string fromNumber { get; set; } = "(864) 651-9551";
+    }
+}
